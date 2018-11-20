f0df88f18e28c3a672e8dd5ebacdd5b23f8f5f06
francois branciard <francois.branciard@iex.ec>
2018-11-20 15:29:09 +0100

    WIP CI test

--- poa/test/CI-testnet-check.sh
+++ poa/test/CI-testnet-check.sh
@@ -17,15 +17,22 @@ sudo rm -rf PoCo-dev
 sudo rm -rf wallets
 sudo rm -rf parity-deploy
 
+sudo rm -rf /home/ubuntu/iexec-deploy
+sudo rm -rf /home/ubuntu/PoCo-dev
+sudo rm -rf /home/ubuntu/wallets
+sudo rm -rf /home/ubuntu/parity-deploy
+
 
 git clone https://github.com/iExecBlockchainComputing/iexec-deploy.git
-chmod +x ~/iexec-deploy/poa/test/bootpoatestnetV3.sh
-sudo -i -u ubuntu ~/iexec-deploy/poa/test/bootpoatestnetV3.sh --name CI-TESTNET-CHECK --nodes 2 --gitlogin ${GIT_LOGIN} --gittoken ${GIT_TOKEN}
+mv iexec-deploy /home/ubuntu/
+chown -R ubuntu:ubuntu /home/ubuntu/iexec-deploy
+chmod 755 /home/ubuntu/iexec-deploy/poa/test/bootpoatestnetV3.sh
+sudo -i -u ubuntu /home/ubuntu/iexec-deploy/poa/test/bootpoatestnetV3.sh --name CI-TESTNET-CHECK --nodes 2 --gitlogin ${GIT_LOGIN} --gittoken ${GIT_TOKEN}
 BOOTSTRAP_RESULT=$?
 if [ $BOOTSTRAP_RESULT -eq 0 ]; then
     echo "SUCCESS ! start OK endpoint"
-    sudo docker run -d -v ~/iexec-deploy/poa/test/CI-OK:/var/www:ro -p 9999:8080 trinitronx/python-simplehttpserver
+    sudo docker run -d -v /home/ubuntu/iexec-deploy/poa/test/CI-OK:/var/www:ro -p 9999:8080 trinitronx/python-simplehttpserver
 else
     echo "FAILED ! start KO endpoint"
-    sudo docker run -d -v ~/iexec-deploy/poa/test/CI-KO:/var/www:ro -p 9999:8080 trinitronx/python-simplehttpserver
+    sudo docker run -d -v /home/ubuntu/iexec-deploy/poa/test/CI-KO:/var/www:ro -p 9999:8080 trinitronx/python-simplehttpserver
 fi
