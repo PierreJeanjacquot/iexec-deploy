bc230fb3018eae6557e8cac0a66e16f00b84b38e
francois branciard <francois.branciard@iex.ec>
2018-11-23 12:02:23 +0100

    WIP test

--- poa/CI/bootpoatestnetV3master.sh
+++ poa/CI/bootpoatestnetV3master.sh
@@ -250,12 +250,16 @@ echo "deploy smart contract poa bridges on network"
 git clone -b $REPO_POA_BRIDGE_CONTRACTS https://github.com/poanetwork/poa-bridge-contracts.git
 cd poa-bridge-contracts
 
+# attach docker to parity-deploy network
+cat ${SCRIPT_DIR}/parity-deploy-network.conf >> docker-compose.yml
+
 cp -rf ${SCRIPT_DIR}/poa-bridge-contracts-dev.env ${SCRIPT_DIR}/poa-bridge-contracts-dev.env.ori
 sed -i "s/__ADMIN_WALLET_PRIVATEKEY__/${PKEY}/g" ${SCRIPT_DIR}/poa-bridge-contracts-dev.env
 sed -i "s/__ADMIN_WALLET__/0xabcd1339Ec7e762e639f4887E2bFe5EE8023E23E/g" ${SCRIPT_DIR}/poa-bridge-contracts-dev.env
 sed -i "s/__ERC20_TOKEN_ADDRESS__/${RlcAddress}/g" ${SCRIPT_DIR}/poa-bridge-contracts-dev.env
 
 cp ${SCRIPT_DIR}/poa-bridge-contracts-dev.env deploy/.env
+
 
 ./deploy.sh
 
