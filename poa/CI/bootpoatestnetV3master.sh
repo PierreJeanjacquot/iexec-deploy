5d4dd157b40e0ccb8a71dd1051fcf2eaaac62000
francois branciard <francois.branciard@iex.ec>
2018-12-05 12:34:55 +0100

    test WIP

--- poa/CI/bootpoatestnetV3master.sh
+++ poa/CI/bootpoatestnetV3master.sh
@@ -179,7 +179,7 @@ fi
 
 echo "call parity-deploy script"
 
-./parity-deploy.sh --config aura --name HOME-CHAIN --nodes 1 --expose parity --force-sealing
+./parity-deploy.sh --config aura --name HOME-CHAIN --nodes 1 --expose
 
 
 sed -i 's/0x00Ea169ce7e0992960D3BdE6F5D539C955316432/0x000a9c787a972f70f0903890e266f41c795c4dca/g' deployment/chain/spec.json
@@ -209,7 +209,7 @@ fi
 
 echo "call parity-deploy script"
 
-./parity-deploy.sh --config aura --name FOREIGN-CHAIN --nodes 1  --expose parity --force-sealing
+./parity-deploy.sh --config aura --name FOREIGN-CHAIN --nodes 1  --expose
 
 
 sed -i 's/0x00Ea169ce7e0992960D3BdE6F5D539C955316432/0x000a9c787a972f70f0903890e266f41c795c4dca/g' deployment/chain/spec.json
