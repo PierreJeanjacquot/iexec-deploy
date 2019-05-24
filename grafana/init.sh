083b075e40723d65283c59642b5133ac8d11dbeb
Vladimir Ostapenco <vladost.pro@gmail.com>
2019-05-24 11:39:46 +0200

    Correct the name and buttons

--- grafana/init.sh
+++ grafana/init.sh
@@ -11,12 +11,53 @@ echo "***************"
 # Modifing home dashboard title
 echo "Modifing home dashboard title..."
 cp ./dashboards/viewer/home/home.json ./dashboards/viewer/home/home2.json
-if [ $ACTIVATE_POOL_JOIN == "1" ]; then
-  echo "Pool join activated..."
-  jq ".dashboard.panels[0].content = \"<input type='button' value='Join Worker Pool' id='button' class='navbar-button' onclick=\\\"location.href='/d/JoinWorkerPool';\\\"/><div class='text-center dashboard-header'>\n  <img src='$GRAFANA_HOME_LOGO_PATH' width='$GRAFANA_HOME_LOGO_WIDTH' height='$GRAFANA_HOME_LOGO_HEIGHT' />\n  <span>$GRAFANA_HOME_NAME</span>\n</div>\"" <./dashboards/viewer/home/home2.json >./dashboards/viewer/home/home.json
-else
-  jq ".dashboard.panels[0].content = \"<div class='text-center dashboard-header'>\n  <img src='$GRAFANA_HOME_LOGO_PATH' width='$GRAFANA_HOME_LOGO_WIDTH' height='$GRAFANA_HOME_LOGO_HEIGHT' />\n  <span>$GRAFANA_HOME_NAME</span>\n</div>\"" <./dashboards/viewer/home/home2.json >./dashboards/viewer/home/home.json
-fi
+
+#if [ $ACTIVATE_POOL_JOIN == "1" ]; then
+#  echo "Pool join activated..."
+#  jq ".dashboard.panels[0].content = \"<input type='button' value='Join Worker Pool' id='button' class='navbar-button' onclick=\\\"location.href='/d/JoinWorkerPool';\\\"/><div class='text-center dashboard-header'>\n  <img src='$GRAFANA_HOME_LOGO_PATH' width='$GRAFANA_HOME_LOGO_WIDTH' height='$GRAFANA_HOME_LOGO_HEIGHT' />\n  <span>$GRAFANA_HOME_NAME</span>\n</div>\"" <./dashboards/viewer/home/home2.json >./dashboards/viewer/home/home.json
+#else
+
+jq ".dashboard.panels[0].content = \" \
+<style type='text/css'> \
+  .navbar-button-new {  \
+    background-image: linear-gradient(90deg,#F9C300,#FAE900);  \
+    font-weight: 500;  \
+    padding: 20px 12px;  \
+    line-height: 0px;  \
+    color: #5D4B00;  \
+    border-radius: 5px;  \
+  } \
+  .navbar-button-new:hover { \
+   background: #D2B12A; \
+   color: #5D4B00; \
+  } \
+  .main-div { \
+    width: 250px; \
+    content: ''; \
+    display: table; \
+  } \
+  .element-div { \
+    float: left; \
+    width: 50%; \
+  } \
+  .clear-div { \
+    clear: both; \
+  } \
+</style> \
+<div class='main-div'> \
+  <div class='element-div'> \
+    <input type='button' value='Start A Worker' id='button' class='navbar-button navbar-button-new' onclick=\\\"window.open('https://docs.iex.ec/worker.html');\\\"/> \
+  </div> \
+  <div class='element-div'> \
+    <input type='button' value='Buy Orders' id='button' class='navbar-button navbar-button-new' onclick=\\\"window.open('https://market.iex.ec');\\\"/> \
+  </div> \
+  <div class='clear-div'> </div> \
+</div> \
+<div class='text-center dashboard-header'> \
+  <img src='$GRAFANA_HOME_LOGO_PATH' width='$GRAFANA_HOME_LOGO_WIDTH' height='$GRAFANA_HOME_LOGO_HEIGHT' /> \
+  <span>$GRAFANA_HOME_NAME</span> \
+</div>\""  <./dashboards/viewer/home/home2.json >./dashboards/viewer/home/home.json
+#fi
 
 rm ./dashboards/viewer/home/home2.json
 
@@ -90,7 +131,7 @@ AddHomeDashboard() {
 }
 
 SetHomeDashboard() {
-    
+
     curl -s -H "Content-Type: application/json" \
          -X PUT -d "{\"theme\": \"\",
                      \"homeDashboardId\":$1,
@@ -161,56 +202,56 @@ if [ ! -z $ACTIVATE_POOL_JOIN ] && [ "$ACTIVATE_POOL_JOIN" == "1" ]; then
 
   if [ ! -z "$POOL_JOIN_WORKER_POOLNAME" ] ; then
   	sed -i "s/^WORKER_POOLNAME=.*$/WORKER_POOLNAME=$POOL_JOIN_WORKER_POOLNAME/g" /launch-worker.sh
-  else 
+  else
   	sed -i "s/^WORKER_POOLNAME=.*$/WORKER_POOLNAME=unknown/g" /launch-worker.sh
   fi
 
   if [ ! -z "$POOL_JOIN_DEPOSIT" ] ; then
   	sed -i "s/^DEPOSIT=.*$/DEPOSIT=$POOL_JOIN_DEPOSIT/g" /launch-worker.sh
-  else 
+  else
   	sed -i "s/^DEPOSIT=.*$/DEPOSIT=0/g" /launch-worker.sh
   fi
 
   if [ ! -z "$POOL_JOIN_CHAIN" ] ; then
   	sed -i "s/^CHAIN=.*$/CHAIN=$POOL_JOIN_CHAIN/g" /launch-worker.sh
-  else 
+  else
   	sed -i "s/^CHAIN=.*$/CHAIN=kovan/g" /launch-worker.sh
   fi
 
   if [ ! -z "$POOL_JOIN_MINETHEREUM" ] ; then
   	sed -i "s/^MINETHEREUM=.*$/MINETHEREUM=$POOL_JOIN_MINETHEREUM/g" /launch-worker.sh
-  else 
+  else
   	sed -i "s/^MINETHEREUM=.*$/MINETHEREUM=0.25/g" /launch-worker.sh
   fi
 
   if [ ! -z "$POOL_JOIN_HUBCONTRACT" ] ; then
   	sed -i "s/^HUBCONTRACT=.*$/HUBCONTRACT=$POOL_JOIN_HUBCONTRACT/g" /launch-worker.sh
-  else 
+  else
   	sed -i "s/^HUBCONTRACT=.*$/HUBCONTRACT=0x/g" /launch-worker.sh
   fi
 
   if [ ! -z "$POOL_JOIN_WORKER_DOCKER_IMAGE_VERSION" ] ; then
   	sed -i "s/^WORKER_DOCKER_IMAGE_VERSION=.*$/WORKER_DOCKER_IMAGE_VERSION=$POOL_JOIN_WORKER_DOCKER_IMAGE_VERSION/g" /launch-worker.sh
-  else 
+  else
   	sed -i "s/^WORKER_DOCKER_IMAGE_VERSION=.*$/WORKER_DOCKER_IMAGE_VERSION=latest/g" /launch-worker.sh
   fi
 
   if [ ! -z "$POOL_JOIN_IEXEC_CORE_HOST" ] ; then
   	sed -i "s/^IEXEC_CORE_HOST=.*$/IEXEC_CORE_HOST=$POOL_JOIN_IEXEC_CORE_HOST/g" /launch-worker.sh
-  else 
+  else
   	sed -i "s/^IEXEC_CORE_HOST=.*$/IEXEC_CORE_HOST=mainnet/g" /launch-worker.sh
   fi
 
   if [ ! -z "$POOL_JOIN_IEXEC_SDK_VERSION" ] ; then
     sed -i "s/^IEXEC_SDK_VERSION=.*$/IEXEC_SDK_VERSION=$POOL_JOIN_IEXEC_SDK_VERSION/g" /launch-worker.sh
-  else 
+  else
     sed -i "s/^IEXEC_SDK_VERSION=.*$/IEXEC_SDK_VERSION=latest/g" /launch-worker.sh
   fi
 
 
   if [ ! -z "$POOL_JOIN_IEXEC_CORE_PORT" ] ; then
   	sed -i "s/^IEXEC_CORE_PORT=.*$/IEXEC_CORE_PORT=$POOL_JOIN_IEXEC_CORE_PORT/g" /launch-worker.sh
-  else 
+  else
   	sed -i "s/^IEXEC_CORE_PORT=.*$/IEXEC_CORE_PORT=/g" /launch-worker.sh
   fi
 
