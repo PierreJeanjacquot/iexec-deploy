4509f9a111ba12c88ed9de698b29323e430aa332
francoisbranciard <francois.branciard@iex.ec>
2019-06-24 10:53:16 +0200

    test wip

---
+++ poa/CI/CI-sidechain.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+
+GIT_LOGIN=""
+GIT_TOKEN=""
+TEST_IP=""
+PASSWORD=""
+
+SCRIPT_DIR=`dirname $0`
+
+
+IEXEC_SDK=next
+#overwrite env by .env
+if [ -f ${SCRIPT_DIR}/.env ]
+then
+  export $(egrep -v '^#' ${SCRIPT_DIR}/.env | xargs)
+fi
+
+sudo npm -g install iexec@$IEXEC_SDK
+iexec --version
+
+#clean up
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+docker rmi $(docker images -q) -f
+docker network rm $(docker network ls | grep "bridge" | awk '/ / { print $1 }')
+
+
+sudo rm -rf iexec-deploy
+sudo rm -rf PoCo
+sudo rm -rf PoCo-home-chain
+sudo rm -rf PoCo-foreign-chain
+sudo rm -rf wallets
+sudo rm -rf parity-deploy
+sudo rm -rf parity-deploy-home-chain
+sudo rm -rf parity-deploy-foreign-chain
+sudo rm -rf poa-bridge-contracts
+sudo rm -rf token-bridge
+sudo rm -rf bridge-ui
+sudo rm -rf bridge-monitor
+
+
+sudo rm -rf /home/ubuntu/iexec-deploy
+sudo rm -rf /home/ubuntu/PoCo
+sudo rm -rf /home/ubuntu/PoCo-home-chain
+sudo rm -rf /home/ubuntu/PoCo-foreign-chain
+sudo rm -rf /home/ubuntu/wallets
+sudo rm -rf /home/ubuntu/parity-deploy
+sudo rm -rf /home/ubuntu/parity-deploy-home-chain
+sudo rm -rf /home/ubuntu/parity-deploy-foreign-chain
+sudo rm -rf /home/ubuntu/poa-bridge-contracts
+sudo rm -rf /home/ubuntu/token-bridge
+sudo rm -rf /home/ubuntu/bridge-ui
+sudo rm -rf /home/ubuntu/bridge_data/
+sudo rm -rf /home/ubuntu/parity-ethereum
+sudo rm -rf /home/ubuntu/bridge-monitor
+
+
+npm
+git clone https://github.com/iExecBlockchainComputing/iexec-deploy.git
+mv iexec-deploy /home/ubuntu/
+chown -R ubuntu:ubuntu /home/ubuntu/iexec-deploy
+chmod 755 /home/ubuntu/iexec-deploy/poa/CI/sidechainandbridgenonreg.sh
+sudo -i -u ubuntu /home/ubuntu/iexec-deploy/poa/CI/bootpoa.sh  --password $PASSWORD --gitlogin ${GIT_LOGIN} --gittoken ${GIT_TOKEN} --test-ip ${TEST_IP}
+BOOTSTRAP_RESULT=$?
+if [ $BOOTSTRAP_RESULT -eq 0 ]; then
+    echo "SUCCESS ! start OK endpoint"
+    #docker stop $(docker ps -a -q)
+    sudo docker run -d -v /home/ubuntu/iexec-deploy/poa/CI/CI-OK:/var/www:ro -p 9999:8080 trinitronx/python-simplehttpserver
+else
+    echo "FAILED ! start KO endpoint"
+    #docker stop $(docker ps -a -q)
+    sudo docker run -d -v /home/ubuntu/iexec-deploy/poa/CI/CI-KO:/var/www:ro -p 9999:8080 trinitronx/python-simplehttpserver
+fi
---
+++ poa/CI/sidechainandbridgenonreg.sh
@@ -0,0 +1,431 @@
+#!/bin/sh
+
+SCRIPT_DIR=`dirname $0`
+
+help()  {
+
+  echo "sidechainandbridgesnonreg.sh OPTIONS
+Usage:
+REQUIRED:
+        --gitlogin : login for private repo
+        --gittoken : token for private repa
+OPTIONAL:
+        --stepDuration : delay between 2 blocks. Default: 2 sec
+        --wallets : nb wallet create. Default: 10
+        --eth     : eth amount (in wei) given to wallets Default: 10 ETH
+        --rlc     : rlc amount (in nRLC) given to wallets Default: RLC ETH
+        -h | --help
+  "
+
+}
+
+
+isInteger() {
+  if test ${1} -eq ${1} 2>/dev/null; then
+    return 0
+  fi
+  return 1
+}
+
+#### MAIN
+
+#default value
+STEP_DURATION=2
+NETWORK_ID_HOME=0x11
+NETWORK_ID_FOREIGN=0x12
+PASSWORD_LENGTH=24
+NB_WALLETS=3
+ETH_AMOUNT=10000000000000000000
+RLC_AMOUNT=1000000000000000
+GIT_LOGIN=""
+GIT_TOKEN=""
+TEST_IP=""
+PARITY_DOCKER_VERSION=stable
+REPO_WALLETS_TAG="master"
+REPO_POCO_TAG="master"
+REPO_PARITY_DEPLOY_TAG="master"
+REPO_POA_BRIDGE_CONTRACTS="master"
+REPO_TOKEN_BRIDGE_MONO="master"
+PASSWORD=""
+
+
+ARGS="$@"
+
+if [ $# -lt 2 ]
+then
+  echo "2 REQUIRED arguments needed"
+  help
+  exit 1
+fi
+
+while [ "$1" != "" ]; do
+  case $1 in
+    --wallets )               shift
+      NB_WALLETS=$1
+      ;;
+    --stepDuration )          shift
+      STEP_DURATION=$1
+      ;;
+    --eth )                   shift
+      ETH_AMOUNT=$1
+      ;;
+    --rlc )                   shift
+      RLC_AMOUNT=$1
+      ;;
+    --gitlogin )              shift
+      GIT_LOGIN=$1
+      ;;
+    --gittoken )              shift
+      GIT_TOKEN=$1
+      ;;
+    --test-ip )              shift
+      TEST_IP=$1
+      ;;
+    --repo-wallet-tag )       shift
+      REPO_WALLETS_TAG=$1
+      ;;
+    --repo-poco-tag )       shift
+      REPO_POCO_TAG=$1
+      ;;
+    --repo-parity-deploy-tag )       shift
+      REPO_PARITY_DEPLOY_TAG=$1
+      ;;
+    --repo-poa-bridge-contracts-tag )       shift
+      REPO_POA_BRIDGE_CONTRACTS=$1
+      ;;
+    --repo-token-bridge-tag )       shift
+      REPO_TOKEN_BRIDGE_MONO=$1
+      ;;
+    --password )       shift
+      PASSWORD=$1
+      ;;
+    -h | --help )           help
+      exit
+      ;;
+  esac
+  shift
+done
+
+#overwrite env by .env
+if [ -f ${SCRIPT_DIR}/.env ]
+then
+  export $(egrep -v '^#' ${SCRIPT_DIR}/.env | xargs)
+fi
+
+
+#check mandatory
+if [ -z $GIT_LOGIN ] ; then
+  echo "--gitlogin  arg is mandatory"
+  help
+  exit 1
+fi
+
+if [ -z $GIT_TOKEN ] ; then
+  echo "--gittoken  arg is mandatory"
+  help
+  exit 1
+fi
+
+if [ -z $TEST_IP ] ; then
+  echo "--test-ip  arg is mandatory"
+  help
+  exit 1
+fi
+
+if [ -z $PASSWORD ] ; then
+  echo "--password arg is mandatory"
+  help
+  exit 1
+fi
+
+
+
+#check ${NB_WALLETS} integer
+isInteger ${NB_WALLETS}
+if [ $? -eq 1 ]
+then
+  echo "NB_WALLETS ${NB_WALLETS} must be an integer."
+  exit 1
+fi
+
+isInteger ${STEP_DURATION}
+if [ $? -eq 1 ]
+then
+  echo "STEP_DURATION ${STEP_DURATION} must be an integer."
+  exit 1
+fi
+
+
+echo "git clone ..."
+git clone -b $REPO_WALLETS_TAG  https://"$GIT_LOGIN":"$GIT_TOKEN"@github.com/iExecBlockchainComputing/wallets.git
+git clone -b $REPO_POCO_TAG https://github.com/iExecBlockchainComputing/PoCo.git
+
+git clone -b $REPO_PARITY_DEPLOY_TAG https://github.com/iExecBlockchainComputing/parity-deploy.git
+
+
+##create docker network
+docker network create parity-deploy_default
+
+CURRENT_DIR=$(pwd)
+
+#install ethkey
+#git clone https://github.com/paritytech/parity-ethereum.git
+#cd parity-ethereum
+#cargo build -p ethkey-cli --release
+#./target/release/ethkey --help
+#sudo cp ./target/release/ethkey /usr/local/bin
+#which ethkey
+
+cd $CURRENT_DIR
+cp -rf parity-deploy parity-deploy-home-chain
+cp -rf parity-deploy parity-deploy-foreign-chain
+
+echo "generate parity-deploy-home-chain"
+cd parity-deploy-home-chain
+echo "generate pwd"
+./config/utils/pwdgen.sh -n 1 -l ${PASSWORD_LENGTH}
+if [ $? -eq 1 ]
+then
+  echo "pwdgen.sh  script failed"
+  exit 1
+fi
+
+echo "call parity-deploy script"
+
+echo "target PARITY VERSION :$PARITY_DOCKER_VERSION"
+
+./parity-deploy.sh --config aura --name HOME-CHAIN --nodes 1 --entrypoint "/bin/parity" --release $PARITY_DOCKER_VERSION --expose
+
+
+sed -i 's/0x00Ea169ce7e0992960D3BdE6F5D539C955316432/0xabcd1339Ec7e762e639f4887E2bFe5EE8023E23E/g' deployment/chain/spec.json
+sed -i "s/\"stepDuration\": \"2\"/\"stepDuration\": \"`echo $STEP_DURATION`\"/g" deployment/chain/spec.json
+sed -i "s/\"networkID\" : \"0x11\"/\"networkID\" : \"`echo $NETWORK_ID_HOME`\"/g" deployment/chain/spec.json
+
+#sed -i "s/stable/$PARITY_DOCKER_VERSION/g" docker-compose.yml
+sed -i "s/host1/host-home-chain/g" docker-compose.yml
+
+
+sed -i "s/d \/home\/parity\/data/d \/home\/parity\/data --force-sealing --logging sync=info,snapshot=debug,txqueue=trace,tx=trace,tx_filter=trace,rpc=trace/g" docker-compose.yml
+
+
+sed -i "s/host1/host-home-chain/g" deployment/chain/reserved_peers
+#cat ${SCRIPT_DIR}/parity-deploy-network.conf >> docker-compose.yml
+echo "docker-compose up -d ..."
+docker-compose up -d
+
+
+cd $CURRENT_DIR
+cd parity-deploy-foreign-chain
+echo "generate parity-deploy-foreign-chain"
+echo "generate pwd"
+./config/utils/pwdgen.sh -n 1 -l ${PASSWORD_LENGTH}
+if [ $? -eq 1 ]
+then
+  echo "pwdgen.sh  script failed"
+  exit 1
+fi
+
+echo "call parity-deploy script"
+echo "target PARITY VERSION :$PARITY_DOCKER_VERSION"
+./parity-deploy.sh --config aura --name FOREIGN-CHAIN --nodes 1 --entrypoint "/bin/parity" --release $PARITY_DOCKER_VERSION  --expose
+
+
+sed -i 's/0x00Ea169ce7e0992960D3BdE6F5D539C955316432/0xabcd1339Ec7e762e639f4887E2bFe5EE8023E23E/g' deployment/chain/spec.json
+sed -i "s/\"stepDuration\": \"2\"/\"stepDuration\": \"`echo $STEP_DURATION`\"/g" deployment/chain/spec.json
+sed -i "s/\"networkID\" : \"0x11\"/\"networkID\" : \"`echo $NETWORK_ID_FOREIGN`\"/g" deployment/chain/spec.json
+
+
+
+
+#sed -i "s/stable/$PARITY_DOCKER_VERSION/g" docker-compose.yml
+echo "change redirect port"
+sed -i "s/- 8080/- 9080/g" docker-compose.yml
+sed -i "s/- 8180/- 9180/g" docker-compose.yml
+sed -i "s/- 8545/- 9545/g" docker-compose.yml
+sed -i "s/- 8546/- 9546/g" docker-compose.yml
+sed -i "s/- 30303/- 40303/g" docker-compose.yml
+sed -i "s/host1/host-foreign-chain/g" docker-compose.yml
+sed -i "s/d \/home\/parity\/data/d \/home\/parity\/data --force-sealing --logging sync=info,snapshot=debug,txqueue=trace,tx=trace,tx_filter=trace,rpc=trace/g" docker-compose.yml
+
+sed -i "s/host1/host-foreign-chain/g" deployment/chain/reserved_peers
+sed -i "s/30303/40303/g" deployment/chain/reserved_peers
+
+#cat ${SCRIPT_DIR}/parity-deploy-network.conf >> docker-compose.yml
+echo "docker-compose up -d ..."
+docker-compose up -d
+
+
+
+
+#ADMIN_PRIVATE_KEY=$(cat ../wallets/wallets/wallet-abc.json | grep privateKey | cut -d ":" -f2 | cut -d "," -f1 | sed 's/\"//g' | sed 's/ //g' | cut  -c3-)
+iexec init --password $PASSWORD --force
+ADMIN_PRIVATE_KEY=$(iexec wallet show --show-private-key --keystoredir /home/ubuntu/wallets/wallets --wallet-file wallet-abc.json --password $PASSWORD --raw | jq '.wallet.privateKey' | sed 's/\"//g' | cut  -c3-)
+
+ADMIN_ADDRESS=$(cat ../wallets/wallets/wallet-abc.json | grep address | cut -d ":" -f2 | cut -d "," -f1 | sed 's/\"//g' | sed 's/ //g')
+
+#echo "ADMIN_PRIVATE_KEY is $ADMIN_PRIVATE_KEY"
+echo "ADMIN_ADDRESS is $ADMIN_ADDRESS"
+
+cd $CURRENT_DIR
+cp -rf PoCo PoCo-home-chain
+cd PoCo-home-chain
+#sed -i "s/\^5.0.0-beta.2/5.0.0-beta.2/g" package.json
+#git checkout ABILegacy
+#npm install truffle-hdwallet-provider@1.0.0-web3one.3
+npm i
+
+#npm install truffle@beta
+
+#copy existing truffle.js
+cp truffle.js truffle.ori
+
+
+sed "s/__PRIVATE_KEY__/\"${ADMIN_PRIVATE_KEY}\"/g" ${SCRIPT_DIR}/truffleV3.tmpl > truffle.js
+
+
+echo "launch truffle migrate"
+./node_modules/.bin/truffle --version
+rm -rf build
+rm -rf truffle.log
+./node_modules/.bin/truffle migrate --network localHomeChain | tee -a "truffle.log" 
+
+if [ $? -eq 0 ]
+then
+  echo "truffle migrate success!"
+else
+  echo "truffle migrate FAILED !"
+  exit 1
+fi
+
+echo "get RLC contract from truffle.log "
+cat truffle.log | grep "RLC deployed at address:"
+
+IexecHubAddress=$(cat build/contracts/IexecHub.json | grep '"address":' | cut -d ":" -f2 | cut -d "," -f1 | sed 's/\"//g' | sed 's/ //g')
+#RlcAddress=$(cat build/contracts/RLC.json | grep '"address":' | cut -d ":" -f2 | cut -d "," -f1 | sed 's/\"//g' | sed 's/ //g')
+echo "get RLC contract from truffle.log "
+RlcAddress=$(cat truffle.log | grep "RLC deployed at address:" | cut -d ":" -f2 | sed 's/ //g')
+echo "RlcAddress $RlcAddress"
+
+if [ -z $IexecHubAddress ]
+then
+  "IexecHubAddress is not set"
+  exit 1
+fi
+echo "IexecHubAddress is $IexecHubAddress"
+
+cd $CURRENT_DIR
+cp -rf PoCo PoCo-foreign-chain
+cd PoCo-foreign-chain
+#sed -i "s/\^5.0.0-beta.2/5.0.0-beta.2/g" package.json
+#git checkout ABILegacy
+npm i
+#npm install truffle-hdwallet-provider@1.0.0-web3one.3
+#npm install truffle@beta
+
+
+
+#copy existing truffle.js
+cp truffle.js truffle.ori
+
+
+sed "s/__PRIVATE_KEY__/\"${ADMIN_PRIVATE_KEY}\"/g" ${SCRIPT_DIR}/truffleV3.tmpl > truffle.js
+
+
+echo "launch truffle migrate"
+./node_modules/.bin/truffle --version
+rm -rf build
+rm -rf truffle.log
+./node_modules/.bin/truffle migrate --network localForeignChain | tee -a "truffle.log" 
+
+if [ $? -eq 0 ]
+then
+  echo "truffle migrate success!"
+else
+  echo "truffle migrate FAILED !"
+  exit 1
+fi
+
+
+IexecHubAddress=$(cat build/contracts/IexecHub.json | grep '"address":' | cut -d ":" -f2 | cut -d "," -f1 | sed 's/\"//g' | sed 's/ //g')
+#RlcAddress=$(cat build/contracts/RLC.json | grep '"address":' | cut -d ":" -f2 | cut -d "," -f1 | sed 's/\"//g' | sed 's/ //g')
+echo "get RLC contract from truffle.log "
+RlcAddress=$(cat truffle.log | grep "RLC deployed at address:" | cut -d ":" -f2 | sed 's/ //g')
+echo "RlcAddress $RlcAddress"
+
+if [ -z $IexecHubAddress ]
+then
+  "IexecHubAddress is not set"
+  exit 1
+fi
+echo "IexecHubAddress is $IexecHubAddress"
+
+
+
+cd $CURRENT_DIR
+cd wallets
+
+# set the right IexecHub find in PoCo-dev/build/contracts/IexecClerk.json contract address
+sed -i "s/0x60E25C038D70A15364DAc11A042DB1dD7A2cccBC/${IexecHubAddress}/g" chain.json
+
+
+iexec --version
+#workaround iexec-sdk :
+mkdir -p ~/.ethereum/keystore
+#end of workaround
+
+#* in network id
+sed -i "s/\"17\"/\"*\"/g" chain.json
+
+# richman used in topUpWallets on homechain
+./topUpWallets --base=admin --from=1 --to=${NB_WALLETS} --minETH=${ETH_AMOUNT} --maxETH=${ETH_AMOUNT} --chain=dev --minRLC=${RLC_AMOUNT} --keystoredir=/home/ubuntu/wallets/wallets
+
+sed -i "s/8545/9545/g" chain.json
+
+
+# richman used in topUpWallets on foreinchain
+./topUpWallets --base=admin  --from=1 --to=${NB_WALLETS} --minETH=${ETH_AMOUNT} --maxETH=${ETH_AMOUNT} --chain=dev --minRLC=${RLC_AMOUNT} --keystoredir=/home/ubuntu/wallets/wallets
+
+
+echo "POA test FOREIGN-CHAIN chain and HOME-CHAIN chain is installed and up "
+
+############################################
+#deploy poa smart contract  bridges on network
+############################################
+cd $CURRENT_DIR
+echo "deploy smart contract poa bridges on network"
+
+git clone -b $REPO_POA_BRIDGE_CONTRACTS https://github.com/iExecBlockchainComputing/poa-bridge-contracts.git
+cd poa-bridge-contracts
+
+# attach docker to parity-deploy network
+#cat ${SCRIPT_DIR}/parity-deploy-network.conf >> docker-compose.yml
+
+cp -rf ${SCRIPT_DIR}/poa-bridge-contracts-sidechain-dev.env ${SCRIPT_DIR}/poa-bridge-contracts-sidechain-dev.env.ori
+sed -i "s/__ADMIN_WALLET_PRIVATEKEY__/${ADMIN_PRIVATE_KEY}/g" ${SCRIPT_DIR}/poa-bridge-contracts-sidechain-dev.env
+sed -i "s/__ADMIN_WALLET__/${ADMIN_ADDRESS}/g" ${SCRIPT_DIR}/poa-bridge-contracts-sidechain-dev.env
+sed -i "s/__ERC20_TOKEN_ADDRESS__/${RlcAddress}/g" ${SCRIPT_DIR}/poa-bridge-contracts-sidechain-dev.env
+sed -i "s/__HOME_RPC_URL__/http:\/\/${TEST_IP}:8545/g" ${SCRIPT_DIR}/poa-bridge-contracts-sidechain-dev.env
+sed -i "s/__FOREIGN_RPC_URL__/http:\/\/${TEST_IP}:9545/g" ${SCRIPT_DIR}/poa-bridge-contracts-sidechain-dev.env
+
+
+
+cp ${SCRIPT_DIR}/poa-bridge-contracts-sidechain-dev.env deploy/.env
+rm -f bridgeDeploy.log
+./deploy.sh  | tee bridgeDeploy.log
+
+
+HOME_BRIDGE_ADDRESS=$(cat bridgeDeploy.log | grep "address" | grep -v function | cut -d ":" -f2 | cut -d "," -f1 | sed 's/\"//g' | sed 's/ //g'| awk 'NR == 1')
+HOME_ERC_677=$(cat bridgeDeploy.log | grep "address" | grep -v function | cut -d ":" -f2 | cut -d "," -f1 | sed 's/\"//g' | sed 's/ //g'| awk 'NR == 2')
+FOREIGN_BRIDGE_ADDRESS=$(cat bridgeDeploy.log | grep "address" | grep -v function | cut -d ":" -f2 | cut -d "," -f1 | sed 's/\"//g' | sed 's/ //g'| awk 'NR == 3')
+HOME_START_BLOCK=$(cat bridgeDeploy.log | grep "deployedBlockNumber" | cut -d ":" -f2 | cut -d "," -f1 | sed 's/\"//g' | sed 's/ //g'| awk 'NR == 1')
+FOREIGN_START_BLOCK=$(cat bridgeDeploy.log | grep "deployedBlockNumber" | cut -d ":" -f2 | cut -d "," -f1 | sed 's/\"//g' | sed 's/ //g'| awk 'NR == 2')
+
+
+
+#clean
+
+cd $CURRENT_DIR
+rm -rf parity-ethereum
+rm -rf PoCo
+rm -rf parity-deploy
+exit 0
